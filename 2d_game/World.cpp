21966a681b2626cf3fbd25f946a3fcfcf261b4ea
lukkelele <encryptedluke@protonmail.com>
2022-07-09 22:46:20 +0200

    C++ testing

--- 2d_game/World.cpp
+++ 2d_game/World.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 class World {
 
-    int X[10];
-    int Y[10];
-    int XY[10];
+    int XY[10][10] = {0};
+    int P[10] = {0};
 
-    protected: World() {
-        this->XY[10] = 0;
+    public: World() {
+     this->P[0] = 5;
+     this->P[1] = 29;
+     this->P[5] = 5;
     }
 
-    private: int* createGrid() {
-        int XY[10];
-
-        return 0;
+    void showGrid() {
+        int x = sizeof(this->XY[0])/sizeof(*this->XY[0]);
+        int y = sizeof(this->XY[1])/sizeof(*this->XY[1]);
+        for (int i=0 ; i < x ; i++) {
+            for (int k=0 ; k < y ; k++) {
+                cout << " " << this->XY[i][k];
+            }
+            cout << "\n";
+        }
     }
 
 };
+
+int main() {
+
+    World world = World();
+    world.showGrid();
+    
+    cout << "\n";
+    return 0;
+}
