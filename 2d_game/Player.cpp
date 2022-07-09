292e23735af30314d1f37076d0cdfe8460c8acd8
lukkelele <encryptedluke@protonmail.com>
2022-07-09 22:01:13 +0200

    Game

---
+++ 2d_game/Player.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+class Player {
+
+    private: int posX, posY;
+    private: bool whitelist, op;
+
+    public: Player() {
+        cout << "Player object created\n";
+        this->posX = 0;
+        this->posY = 0;
+        
+    }
+
+
+
+};
---
+++ 2d_game/World.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+class World {
+
+    int X[10];
+    int Y[10];
+    int XY[10];
+
+    protected: World() {
+        this->XY[10] = 0;
+    }
+
+    private: int* createGrid() {
+        int XY[10];
+
+        return 0;
+    }
+
+};
