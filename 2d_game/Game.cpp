3d126d16674cfd26beacd73e5113454a1f471073
lukkelele <encryptedluke@protonmail.com>
2022-07-10 20:32:32 +0200

    Upd

--- 2d_game/Game.cpp
+++ 2d_game/Game.cpp
@@ -1,3 +1,40 @@
 #include <iostream>
+#include "Player.h"
+#include "World.h"
 using namespace std;
+
+
+class Game {
+
+  public: World world;
+  private: Player player;
+
+  public: Game() {
+            cout << "Game constructor called!\n";
+            this->world = this->createWorld();
+          }
+
+  public: World createWorld() {
+             World world = World();
+             return world;
+           }
+
+
+};
+
+
+
+int main() {
+  cout << "\nStarting game!\n";
+  Game game = Game();
+  int coordinates[2] = {2,2};
+  game.world.setPlayer(coordinates);
+  game.world.showGrid();
+  return 0;
+}
+
+
+
+
+
 
